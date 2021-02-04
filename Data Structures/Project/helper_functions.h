1a612aece2d9e3bdca683534d71ae8152201f43b
SimeonHristov99 <s.e.hristov99@gmail.com>
2021-02-04 21:26:14 +0200

    move project

--- Data Structures/Project/helper_functions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-/// Really those needn't be functions but for clarity, I separated them.
-
-bool is_character(const char c);
-
-bool is_digit(const char c);
--- Data Structures/Project/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Lexer.h"
-
-int main()
-{
-	std::cout << "Write \"e0\" to exit program.\n\n";
-	run(std::cin, std::cout);
-
-	return 0;
-}
